abaa5a35de94c1d436be742c000e502446f0920a
YoKamaru <ka.ka.maru55+git@gmail.com>
2011-01-14 01:25:06 +0900

    edy残高表示プログラムを作成

---
+++ C/edyview.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libpafe/libpafe.h>
+
+int main(int argc, char* argv[])
+{
+    pasori *p;
+    felica *f;
+    uint8 data[16];
+
+    p = pasori_open();
+    if (!p)
+    {
+        fprintf(stderr, "Failed to open PaSoLi.\n");
+        return EXIT_FAILURE;
+    }
+    pasori_init(p);
+    f = felica_polling(p, FELICA_POLLING_EDY, 0, 0);
+
+    if (f && !felica_read_single(f, FELICA_SERVICE_EDY_HISTORY, 0, 0, data))
+    {
+        printf("%5d Yen\n", (data[14] << 8) + data[15]);
+    }
+    else
+    {
+        fprintf(stderr, "Failed to find your edy.\n");
+        return EXIT_FAILURE;
+    }
+
+    free(f);
+    pasori_close(p);
+
+    return EXIT_SUCCESS;
+}
